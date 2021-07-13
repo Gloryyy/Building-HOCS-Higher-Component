f1091ce32990299f62ac6e7c3996a9ca719c5cf0
gloryyy <majdwarrrak@gmail.com>
2021-07-13 21:46:32 +0200

    building higher components with hocs, creating api and fetching data by using class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UserProfile from "./components/user-profile/user-profile.component";
+import "./App.scss";
+import UserList from "./components/user-list/user-list.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserList dataSource="https://jsonplaceholder.typicode.com/users" />
+      <UserProfile
+        dataSource="https://jsonplaceholder.typicode.com/posts"
+        name="User"
+        email="user@gmail.com"
+      />
     </div>
   );
 }
